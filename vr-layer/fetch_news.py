5aa64de26a56b2299d06c4b13f345d240ae3a2cf
likeMike001 <like.mike.007.382@gmail.com>
2025-11-08 17:26:30 -0700

    updated fetch news to save news as csv in data

--- vr-layer/fetch_news.py
+++ vr-layer/fetch_news.py
@@ -1,6 +1,7 @@
 # fetch_news.py
 from news_extractor import generate_synthetic_news
-
+import pandas as pd 
+from datetime import datetime
 
 def main():
     topic = "Ethereum"
@@ -8,16 +9,14 @@ def main():
 
     articles = generate_synthetic_news(topic, num_articles)
 
-    for article in articles:
-        print(f"ID:           {article['id']}")
-        print(f"Topic:        {article['topic']}")
-        print(f"Headline:     {article['headline']}")
-        print(f"Source:       {article['source']}")
-        print(f"Published At: {article['published_at']}")
-        print("Body (first 300 chars):")
-        print(article['body'][:300])
-        print("-" * 80)
-
-
+    df = pd.DataFrame(articles)
+    
+    time_stamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    file_path = f"../data/synthetic_news_{topic}_{time_stamp}.csv"
+    
+    df.to_csv(file_path, index=False)
+    print(f"Synthetic news articles saved to {file_path}")
+   
+   
 if __name__ == "__main__":
     main()
